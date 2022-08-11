970d7a68bcbc99c7707ac20d922387c5158616fc
Bianca Ciorobea <ciorobea.b@northeastern.edu>
2022-08-11 11:39:39 -0400

    Added initial applyFilter method

---
+++ src/main/java/com/bc/demoproj/uploadingfiles/storage/StorageException.java
@@ -0,0 +1,12 @@
+package com.bc.demoproj.uploadingfiles.storage;
+
+public class StorageException extends RuntimeException {
+
+	public StorageException(String message) {
+		super(message);
+	}
+
+	public StorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
