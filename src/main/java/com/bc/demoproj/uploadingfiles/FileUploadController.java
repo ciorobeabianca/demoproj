6f928c023c2473a27af49f030a6e42a58adf6514
Bianca Ciorobea <ciorobea.b@northeastern.edu>
2022-08-15 12:15:13 -0400

    Added the applyFilter(blur) method and brighten method

--- src/main/java/com/bc/demoproj/uploadingfiles/FileUploadController.java
+++ src/main/java/com/bc/demoproj/uploadingfiles/FileUploadController.java
@@ -84,8 +84,8 @@ public class FileUploadController {
   private Resource applyFilter(Resource originalResource) {
     try {
       InputStream originalInputStream = originalResource.getInputStream();
-      //InputStream filteredInputStream = applyFilter(originalInputStream);
-      return new InputStreamResource(originalInputStream);
+      InputStream filteredInputStream = applyFilter(originalInputStream);
+      return new InputStreamResource(filteredInputStream);
     } catch (IOException exception) {
       throw new RuntimeException(exception);
     }
@@ -102,11 +102,101 @@ public class FileUploadController {
   }
 
   private BufferedImage applyFilter(BufferedImage originalImage){
-    BufferedImage filteredImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), originalImage.getType());
+    BufferedImage filteredImage = new BufferedImage(originalImage.getWidth(),
+            originalImage.getHeight(), originalImage.getType());
+
+    Color[] color;
+    // Setting dimensions for the image to be processed
+    int i = 0;
+    int max = 400, rad = 10;
+    int a1 = 0, r1 = 0, g1 = 0, b1 = 0;
+    color = new Color[max];
+
+    int x = 1, y = 1, x1, y1, ex = 5, d = 0;
+
+    // Running nested for loops for each pixel
+    // and blurring it
+    for (x = rad; x < originalImage.getHeight() - rad; x++) {
+      for (y = rad; y < originalImage.getWidth() - rad; y++) {
+        for (x1 = x - rad; x1 < x + rad; x1++) {
+          for (y1 = y - rad; y1 < y + rad; y1++) {
+            color[i++] = new Color(
+                    originalImage.getRGB(y1, x1));
+          }
+        }
+
+        // Smoothing colors of image
+        i = 0;
+        for (d = 0; d < max; d++) {
+          a1 = a1 + color[d].getAlpha();
+        }
+
+        a1 = a1 / (max);
+        for (d = 0; d < max; d++) {
+          r1 = r1 + color[d].getRed();
+        }
+
+        r1 = r1 / (max);
+        for (d = 0; d < max; d++) {
+          g1 = g1 + color[d].getGreen();
+        }
+
+        g1 = g1 / (max);
+        for (d = 0; d < max; d++) {
+          b1 = b1 + color[d].getBlue();
+        }
+
+        b1 = b1 / (max);
+        int sum1 = (a1 << 24) + (r1 << 16)
+                + (g1 << 8) + b1;
+        filteredImage.setRGB(y, x, (int)(sum1));
+      }
+    }
+
     Graphics graphics = filteredImage.getGraphics();
-    graphics.drawImage(originalImage, 0, 0, null);
+    graphics.drawImage(filteredImage, 0, 0, null);
     graphics.dispose();
     return filteredImage;
 
+  }
+
+  private BufferedImage brighten(BufferedImage originalImage) {
+    BufferedImage filteredImage = new BufferedImage(originalImage.getWidth(),
+            originalImage.getHeight(), originalImage.getType());
+
+    int i = 0, j =0, r, b, g;
+    int max = 400, rad = 30;
+
+    Color color;
+    Color currentCol;
+
+    for (i = 0; i < originalImage.getHeight(); i++) {
+      for (j = 0; j < originalImage.getWidth(); j++) {
+        currentCol = new Color(
+                originalImage.getRGB(j,i));
+        r = currentCol.getRed() + rad;
+        g = currentCol.getGreen() + rad;
+        b = currentCol.getBlue() + rad;
+
+        if (r > 255) {
+          r = 255;
+        }
+
+        if (g > 255) {
+          g = 255;
+        }
+
+        if (b > 255) {
+          b = 255;
+        }
+
+        color = new Color(r,g,b);
+        filteredImage.setRGB(j,i,color.getRGB());
+      }
+    }
+    Graphics graphics = filteredImage.getGraphics();
+    graphics.drawImage(filteredImage, 0, 0, null);
+    graphics.dispose();
+    return filteredImage;
   }
 }
